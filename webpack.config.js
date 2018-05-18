7d408915ebe4c0a2a324dd31d8cfd81eb9f2699b
AJ Kahle <ajkahle@gmail.com>
2018-05-18 15:16:14 -0500

    maybe this works?

--- webpack.config.js
+++ webpack.config.js
@@ -111,7 +111,7 @@ module.exports = {
   output: {
       path: path.resolve(__dirname, 'public'),
       filename: 'bundle.js',
-      publicPath: '/public/',
+      publicPath: './',
   },
   devServer: {
     historyApiFallback: true,
