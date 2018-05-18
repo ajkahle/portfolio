ea5e50bd9bc74799b179a5297b9826c79689bdbb
AJ Kahle <ajkahle@gmail.com>
2018-05-18 15:24:56 -0500

    maybe this works?

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 var express = require('express');
 var app = express();
-app.use(express.static(__dirname + '/build'));
+app.use(express.static(__dirname + '/public'));
 app.listen(process.env.PORT || 8080);
