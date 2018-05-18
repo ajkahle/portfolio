a7ff15586c1d4b54180536ce7255975c2b42a11f
AJ Kahle <ajkahle@gmail.com>
2018-05-18 14:20:28 -0500

    webpack fix

--- index.js
+++ index.js
@@ -1,20 +1,11 @@
-const Server = require('./server.js')
-const port = (process.env.PORT || 8080)
-const app = Server.app()
+var path = require('path');
+var express = require('express');
 
-if (process.env.NODE_ENV !== 'production') {
-  const webpack = require('webpack')
-  const webpackDevMiddleware = require('webpack-dev-middleware')
-  const webpackHotMiddleware = require('webpack-hot-middleware')
-  const config = require('./webpack.dev.config.js')
-  const compiler = webpack(config)
+var app = express();
 
-  app.use(webpackHotMiddleware(compiler))
-  app.use(webpackDevMiddleware(compiler, {
-    noInfo: true,
-    publicPath: config.output.publicPath
-  }))
-}
+app.use(express.static(path.join(__dirname, 'dist')));
+app.set('port', process.env.PORT || 8080);
 
-app.listen(port)
-console.log(`Listening at http://localhost:${port}`)
+var server = app.listen(app.get('port'), function() {
+  console.log('listening on port ', server.address().port);
+});
