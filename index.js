e5e8ef7a04f86c91611212897e45cfa4b1f52411
AJ Kahle <ajkahle@gmail.com>
2018-05-23 10:17:14 -0500

    routing fix

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 var express = require('express');
 var app = express();
 app.use(express.static(__dirname + '/public'));
+
+app.get('*', function(request, response) {
+  response.sendFile(__dirname + '/public/index.html');
+});
+
 app.listen(process.env.PORT || 8080);
